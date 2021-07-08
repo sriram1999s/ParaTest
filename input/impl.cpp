c8dc899c069f7622dbf99f2bb45559595b71f8d1
sriram1999s <sriramsub7@gmail.com>
2021-07-08 19:53:39 +0530

    restructure; added input

---
+++ input/impl.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include<iostream>
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
+
+int mod(int a, int b) {
+  return a % b;
+}
