3d91d91a1b39e7f8a38cc404a8a2ca7492959159
TheCodeYoda <smurf61988@gmail.com>
2021-07-08 21:33:27 +0530

    restructure

---
+++ cpp_files/main.cpp
@@ -0,0 +1,17 @@
+#include "tests.hpp"
+#include<iostream>
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
+
+int mod(int a, int b) {
+  return a % b;
+}
+int main() {
+	test1();
+}
