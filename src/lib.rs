74f3be46170f5f4a12b0550b3c6b961aad7e7fc2
TheCodeYoda <smurf61988@gmail.com>
2021-07-10 12:21:13 +0530

    made it multithreaded

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-// use rayon::prelude::*;
+use rayon::prelude::*;
 
 use termion::color;
 
 use regex::Regex;
 
+use std::env;
 use std::fs;
 use std::process::Command;
-use std::env;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 pub struct TestFunctions {
     list: Vec<String>,
@@ -45,20 +46,20 @@ impl TestFunctions {
 
     // run all tests /* currently sequential */
     pub fn run_tests(&self, tests_path: &str, impl_file: &str) {
-        let mut no_passed_tests = 0;
-        let mut no_failed_tests = 0;
-        let mut no_run_tests = 0;
+        let no_passed_tests = AtomicUsize::new(0);
+        let no_failed_tests = AtomicUsize::new(0);
+        let no_run_tests = AtomicUsize::new(0);
         println!("Running tests...\n");
-        self.list.iter().for_each(|func| {
-
+        self.list.par_iter().for_each(|func| {
             let path = TestFunctions::get_path().unwrap();
-            let mut main_file = String::from(format!("#include \"{}/cpp_files/tests.hpp\"\n", path).as_str());
+            let mut main_file =
+                String::from(format!("#include \"{}/cpp_files/tests.hpp\"\n", path).as_str());
             main_file.push_str(impl_file);
             main_file.push_str(format!("int main() {{\n\t{}();\n}}", func).as_str());
 
             let path = TestFunctions::get_path().unwrap();
 
-            let main_path = String::from(path) + "/cpp_files/main.cpp";
+            let main_path = String::from(path) + format!("/cpp_files/{}_main.cpp", func).as_str();
             let main_path = main_path.as_str();
 
             fs::write(main_path, main_file.as_str())
@@ -67,27 +68,61 @@ impl TestFunctions {
             let output = Command::new("g++")
                 .arg(main_path)
                 .arg(tests_path)
+                .arg("-o")
+                .arg(format!("{}.out", func).as_str())
                 .output()
                 .expect("failed to compile");
 
             assert!(output.status.success());
 
-            let exec = Command::new("./a.out").output().expect("Error when running exec");
+            let exec = Command::new(format!("./{}.out", func).as_str())
+                .output()
+                .expect("Error when running exec");
 
             match exec.status.success() {
-                true => { println!("{} {}PASSED{} : was completed successfully!\n", func, color::Fg(color::LightGreen), color::Fg(color::Reset)); no_passed_tests += 1; no_run_tests += 1;},
-                false => { println!("{} {}FAILED{} : {}", func, color::Fg(color::LightRed), color::Fg(color::Reset),String::from_utf8_lossy(&exec.stderr)); no_failed_tests += 1; no_run_tests += 1;},
+                true => {
+                    println!(
+                        "{} {}PASSED{} : was completed successfully!\n",
+                        func,
+                        color::Fg(color::LightGreen),
+                        color::Fg(color::Reset)
+                    );
+                    no_passed_tests.fetch_add(1, Ordering::Relaxed);
+                    no_run_tests.fetch_add(1, Ordering::Relaxed);
+                }
+                false => {
+                    println!(
+                        "{} {}FAILED{} : {}",
+                        func,
+                        color::Fg(color::LightRed),
+                        color::Fg(color::Reset),
+                        String::from_utf8_lossy(&exec.stderr)
+                    );
+                    no_failed_tests.fetch_add(1, Ordering::Relaxed);
+                    no_run_tests.fetch_add(1, Ordering::Relaxed);
+                }
             }
 
+            let path = TestFunctions::get_path().unwrap();
+            let path = path + format!("/cpp_files/{}_main.cpp", func).as_str();
             let clean = Command::new("rm")
-                .arg("a.out")
+                .arg(format!("{}.out", func).as_str())
+                .arg(path.as_str())
                 .output()
                 .expect("failed to remove binary");
 
             assert!(clean.status.success());
-
         });
-        println!("Summary:\n\nTotal no of tests run : {}, total {}PASSED{} : {}, total {}FAILED{} : {}", no_run_tests, color::Fg(color::LightGreen), color::Fg(color::Reset), no_passed_tests, color::Fg(color::LightRed), color::Fg(color::Reset), no_failed_tests);
+        println!(
+            "Summary:\n\nTotal no of tests run : {}, total {}PASSED{} : {}, total {}FAILED{} : {}",
+            no_run_tests.into_inner() as u32,
+            color::Fg(color::LightGreen),
+            color::Fg(color::Reset),
+            no_passed_tests.into_inner() as u32,
+            color::Fg(color::LightRed),
+            color::Fg(color::Reset),
+            no_failed_tests.into_inner() as u32
+        );
     }
 }
 
@@ -96,8 +131,9 @@ pub fn modify_test_spec_file(tests_file_path: &str, interface: &str) {
     let mut header = format!("#include \"{}\"\n", interface);
     let pat = Regex::new(header.as_str()).unwrap();
 
-    let tests_file = fs::read_to_string(tests_file_path).expect("Something went wrong reading the test specification file");
-    if ! pat.is_match(tests_file.as_str()) {
+    let tests_file = fs::read_to_string(tests_file_path)
+        .expect("Something went wrong reading the test specification file");
+    if !pat.is_match(tests_file.as_str()) {
         header.push_str(tests_file.as_str());
         fs::write(tests_file_path, header).expect("Something went wrong when modifying tests spec");
     }
